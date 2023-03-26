19b269ac354517d44d26d27cc946d77837d470e2
veladam01 <veladam01@gmail.com>
2023-03-26 21:03:30 +0200

    Created GenericSensor.cs

---
+++ src/AutomatedCar/SystemComponents/GenericSensor.cs
@@ -0,0 +1,38 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public abstract class GenericSensor : SystemComponent
+    {
+
+
+        public GenericSensor(VirtualFunctionBus virtualFunctionBus,
+                    Vector2 carAnchorPoint,
+                    double fOV, double viewDistance,
+                    IEnumerable<WorldObjectType> worldObjectTypesFilter)
+                        : base(virtualFunctionBus)
+        {
+            CarAnchorPoint = carAnchorPoint;
+            FOV = fOV;
+            ViewDistance = viewDistance;
+            WorldObjectTypesFilter = worldObjectTypesFilter;
+        }
+
+        public Vector2 CarAnchorPoint { get => carAnchorPoint; set => carAnchorPoint = value; }
+        public double FOV { get => fov; set => fov = value; }
+        public double ViewDistance { get => viewDistance; set => viewDistance = value; }
+        public IEnumerable<WorldObjectType> WorldObjectTypesFilter { get => worldObjectTypesFilter; set => worldObjectTypesFilter = value; }
+
+        protected Vector2 carAnchorPoint;
+        protected double fov;
+        protected double viewDistance;
+        protected IEnumerable<WorldObjectType> worldObjectTypesFilter;
+
+    }
+}
