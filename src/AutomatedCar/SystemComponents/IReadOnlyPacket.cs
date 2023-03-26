79475a68e87dffe7974677845c442385dcf25f47
veladam01 <veladam01@gmail.com>
2023-03-26 21:06:23 +0200

    IReadOnlyPacket.cs created

---
+++ src/AutomatedCar/SystemComponents/IReadOnlyPacket.cs
@@ -0,0 +1,14 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IReadOnlyPacket
+    {
+        IReadOnlyCollection<WorldObject> WorldObjectsDetected { get; }
+    }
+}
