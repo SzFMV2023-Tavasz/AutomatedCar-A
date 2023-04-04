27a948ac4d12af465ba2e6289025d9bf82a28fc1
Kristof0101 <varga.kris@stud.uni-obuda.hu>
2023-04-04 14:06:11 +0200

    IGearboxInterface rename to IGearBoxPacket

---
+++ src/AutomatedCar/SystemComponents/Packets/IGearboxPacket.cs
@@ -0,0 +1,16 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IGearboxPacket
+    {
+        bool ShiftInProgress { get; }
+
+        byte InnerGear { get; }
+        
+    }
+}
