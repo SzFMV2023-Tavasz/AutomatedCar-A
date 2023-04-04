a58b0a14fff973c4b187f4b249f619c356d5c6ea
Kristof0101 <varga.kris@stud.uni-obuda.hu>
2023-04-04 14:06:47 +0200

    GearBoxPacket created

---
+++ src/AutomatedCar/SystemComponents/Packets/GearBoxPacket.cs
@@ -0,0 +1,27 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GearBoxPacket : ReactiveObject, IGearboxPacket
+    {
+
+        private bool shiftInProgress;
+        private byte innerGear;
+
+
+        public bool ShiftInProgress {
+
+            get { return shiftInProgress; }
+        }
+
+        public byte InnerGear
+        {
+            get { return innerGear; }
+        }
+    }
+}
