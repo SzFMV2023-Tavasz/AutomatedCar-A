28d966ffb9344fadbfc4979b704e0c0e9a76e793
Kristof0101 <varga.kris@stud.uni-obuda.hu>
2023-04-15 17:04:24 +0200

    fields added

--- src/AutomatedCar/SystemComponents/Packets/GearBoxPacket.cs
+++ src/AutomatedCar/SystemComponents/Packets/GearBoxPacket.cs
@@ -1,12 +1,22 @@
 ﻿namespace AutomatedCar.SystemComponents.Packets
 {
+    using ReactiveUI;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    internal class GearBoxPacket
+    public class GearBoxPacket : ReactiveObject, IGearboxInterface
     {
+
+        private bool shiftInProgress;
+
+        private byte innerGear;
+
+
+        public bool ShiftInProgress { get;  }
+
+        public byte InnerGear { get; }
     }
 }
