bbb86d29ee3affc9e3199b9e7ca6104562d8bc0e
Básthy Ádám <basthyadi@gmail.com>
2023-04-15 15:15:01 +0200

    Add DetectedObjectInfo

---
+++ src/AutomatedCar/Models/DetectedObjectInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace AutomatedCar.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DetectedObjectInfo
+    {
+        public WorldObject DetectedObject { get; set; }
+
+        public float Distance { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            return (obj is DetectedObjectInfo) && (obj as DetectedObjectInfo).DetectedObject.Equals(this.DetectedObject);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.DetectedObject.GetHashCode();
+        }
+    }
+}
