bf47be4cdbbc02ac9a81f6e74718afae575ab87c
Kristof0101 <varga.kris@stud.uni-obuda.hu>
2023-04-15 18:01:00 +0200

    timer added

--- src/AutomatedCar/SystemComponents/GearBox.cs
+++ src/AutomatedCar/SystemComponents/GearBox.cs
@@ -1,12 +1,42 @@
 ﻿namespace AutomatedCar.SystemComponents
 {
+    using AutomatedCar.SystemComponents.Packets;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    internal class GearBox
+    public class GearBox :SystemComponent
     {
+
+        GearBoxPacket gearBoxPacket = new GearBoxPacket();
+
+        int _RPM;
+
+        System.Timers.Timer aTimer = new System.Timers.Timer();
+        
+        //aTimer.Interval = 5000;
+        //aTimer.Enabled = true;
+
+
+
+        public GearBox(VirtualFunctionBus virtualFunctionBus, int timerInterval) : base(virtualFunctionBus)
+        {
+            virtualFunctionBus.GearboxPacket=gearBoxPacket;
+            this._RPM = virtualFunctionBus.CharacteristicsPacket.RPM;
+            aTimer.Interval= timerInterval;
+            
+        }
+
+        public override void Process()
+        {
+            //ToDo
+        }
+
+        private void Shift()
+        {
+
+        }
     }
 }
