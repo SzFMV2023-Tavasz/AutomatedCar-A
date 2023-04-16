250ebfdacdeef8b37d3638f20fcaa6f47f19e0a9
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2023-04-16 12:05:56 +0200

    Created Pedestrian class.

---
+++ src/AutomatedCar/Models/Pedestrian.cs
@@ -0,0 +1,24 @@
+﻿namespace AutomatedCar.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using global::AutomatedCar.NPC;
+
+    public class Pedestrian : INPC
+    {
+        private NPCManager nPCManager;
+
+        public Pedestrian(NPCManager nPCManager)
+        {
+            this.nPCManager = nPCManager;
+            nPCManager.AddNPC(this);
+        }
+
+        public List<Helpers.PathPoint> PathPoints { get; set; } = new List<Helpers.PathPoint>();
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
