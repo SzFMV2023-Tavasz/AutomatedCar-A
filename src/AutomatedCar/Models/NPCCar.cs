dd7fba28a1151cd486199c4e166f3fb74644691a
tszdaniel@stud.uni-obuda.hu <tszdaniel@stud.uni-obuda.hu>
2023-04-17 18:14:50 +0200

    Move implemented, need to be tested.

--- src/AutomatedCar/Models/NPCCar.cs
+++ src/AutomatedCar/Models/NPCCar.cs
@@ -20,9 +20,35 @@
 
         public List<Helpers.PathPoint> PathPoints { get; set; } = new List<Helpers.PathPoint>();
 
+        public int ActPoint { get; set; } = 0;
+
+        public bool Repeating { get; set; } = true;
+
         public void Move()
         {
-            throw new NotImplementedException();
+            if (!(ActPoint == PathPoints.Count() - 1 && !Repeating))
+            {
+
+                int NextPoint = ActPoint + 1;
+                if (!(NextPoint < PathPoints.Count())) { NextPoint = 0; }
+                int difX = PathPoints[NextPoint].X - this.X;
+                int difY = PathPoints[NextPoint].Y - this.Y;
+                double distance = Math.Sqrt(difX * difX + difY * difY);
+                if (distance <= this.Speed)
+                {
+                    this.X = PathPoints[NextPoint].X;
+                    this.Y = PathPoints[NextPoint].Y;
+                    this.Speed = PathPoints[NextPoint].Speed;
+                    this.Rotation = PathPoints[NextPoint].Rotation;
+                    ActPoint = NextPoint;
+                }
+                else
+                {
+                    double distancePerSpeedRatio = distance / this.Speed;
+                    this.X += (int)Math.Round(difX / distancePerSpeedRatio);
+                    this.Y += (int)Math.Round(difY / distancePerSpeedRatio);
+                }
+            }
         }
     }
 }
