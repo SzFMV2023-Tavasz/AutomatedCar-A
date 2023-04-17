b1aa281c9b55ca8d32c1ce607df507756c15e62b
Martin Pintér <pmrtn@stud.uni-obuda.hu>
2023-04-17 19:39:20 +0200

    Drivechain interface created

---
+++ src/AutomatedCar/SystemComponents/Packets/IDrivechain.cs
@@ -0,0 +1,16 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IDrivechain
+    {
+        public int Speed { get; }
+
+        public Vector2 MotionVector { get; }
+    }
+}
