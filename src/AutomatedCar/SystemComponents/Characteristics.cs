174ba7a416a6e2b10251a0f6f6091fa63337b6f1
Solt Tamás <solttamas1998@gmail.com>
2023-04-18 20:12:05 +0200

    Generated the SystemComponent's methods

--- src/AutomatedCar/SystemComponents/Characteristics.cs
+++ src/AutomatedCar/SystemComponents/Characteristics.cs
@@ -6,7 +6,27 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    internal class Characteristics
+    /// <summary>
+    /// The characteristics class.
+    /// </summary>
+    public class Characteristics : SystemComponent
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Characteristics"/> class.
+        /// </summary>
+        /// <param name="virtualFunctionBus"> The virtual function bus. </param>
+        public Characteristics(VirtualFunctionBus virtualFunctionBus)
+            : base(virtualFunctionBus)
+        {
+
+        }
+
+        /// <summary>
+        /// Calculates the actual RPM and Speed based on the characteristic.
+        /// </summary>
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
