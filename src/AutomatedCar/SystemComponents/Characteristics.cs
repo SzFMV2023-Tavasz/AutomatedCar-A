380d2464935a2e6de6870511cf1c20d3e3c98b0d
andrewhertelendy <hertelendyandras@stud.uni-obuda.hu>
2023-04-18 20:56:32 +0200

    Characteristics roughly implemented

--- src/AutomatedCar/SystemComponents/Characteristics.cs
+++ src/AutomatedCar/SystemComponents/Characteristics.cs
@@ -1,36 +1,86 @@
 ﻿namespace AutomatedCar.SystemComponents
 {
+
     using AutomatedCar.SystemComponents.Packets;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    /// <summary>
-    /// The characteristics class, which calculates the RPM and the speed based on the car's characteristic.
-    /// </summary>
     public class Characteristics : SystemComponent
     {
         private ICharacteristicsInterface characteristicsPacket;
+        private IGearboxInterface gearbox;
+        private IBrakePedalInterface brakePedal;
+        private IGasPedalInterface gasPedal;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Characteristics"/> class.
-        /// </summary>
-        /// <param name="virtualFunctionBus"> The virtual function bus. </param>
-        public Characteristics(VirtualFunctionBus virtualFunctionBus)
+        private const double WheelCircumference = 24 * Math.PI; // 24-inch wheel size converted to circumference
+        private const double BrakeDeceleration = 9; // Deceleration rate 9m/s^2
+
+        public Characteristics(VirtualFunctionBus virtualFunctionBus, IGearboxInterface gearbox, IBrakePedalInterface brakePedal, IGasPedalInterface gasPedal)
             : base(virtualFunctionBus)
         {
             this.characteristicsPacket = new CharacteristicsPacket();
+            this.gearbox = gearbox;
+            this.brakePedal = brakePedal;
+            this.gasPedal = gasPedal;
             virtualFunctionBus.CharacteristicsPacket = this.characteristicsPacket;
         }
 
-        /// <summary>
-        /// Calculates the actual RPM and Speed based on the characteristic.
-        /// </summary>
-        public override void Process()
+        public override void Process(double timeDelta)
         {
-            throw new NotImplementedException();
+            double gearFactor = GetGearFactor(gearbox.InnerGear);
+            double acceleration = CalculateAcceleration(gasPedal.GasPedalState, gearFactor, timeDelta);
+            double deceleration = CalculateDeceleration(brakePedal.BrakePedalState, timeDelta);
+
+            double netAcceleration = acceleration - deceleration;
+            characteristicsPacket.Speed = CalculateSpeed(characteristicsPacket.Speed, netAcceleration, timeDelta);
+            characteristicsPacket.RPM = CalculateRPM(characteristicsPacket.Speed);
+        }
+
+        private double GetGearFactor(int innerGear)
+        {
+            switch (innerGear)
+            {
+                case 1:
+                    return 1.0;
+                case 2:
+                    return 0.8;
+                case 3:
+                    return 0.6;
+                case 4:
+                    return 0.4;
+                case 5:
+                    return 0.2;
+                default:
+                    return 1.0;
+            }
+        }
+
+        private double CalculateAcceleration(int gasPedalState, double gearFactor, double timeDelta)
+        {
+            return Math.Sqrt(gasPedalState * gearFactor) * timeDelta;
+        }
+
+        private double CalculateDeceleration(int brakePedalState, double timeDelta)
+        {
+            if (brakePedalState > 0)
+            {
+                return BrakeDeceleration * (brakePedalState / 100.0) * timeDelta;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private float CalculateSpeed(float currentSpeed, double netAcceleration, double timeDelta)
+        {
+            double newSpeed = currentSpeed + netAcceleration * timeDelta * 3.6; // Convert m/s to km/h
+            return (float)Math.Max(0, newSpeed); // Ensure the speed doesn't go below 0
+        }
+
+        private float CalculateRPM(float speed)
+        {
+            double speedInMetersPerSecond = speed / 3.6; // Convert km/h to m/s
+            double wheelRotationsPerSecond = speedInMetersPerSecond / WheelCircumference;
+            return (float)(wheelRotationsPerSecond * 60); // Convert wheel rotations per second to RPM
         }
     }
 }
