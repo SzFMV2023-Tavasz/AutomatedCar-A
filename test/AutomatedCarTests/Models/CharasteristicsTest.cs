4bc5c8508af706329449ca9fabac75b1d4a2dd6c
Solt Tamás <solttamas1998@gmail.com>
2023-04-19 19:05:32 +0200

    Created CharacteristicsTest.cs

---
+++ test/AutomatedCarTests/Models/CharasteristicsTest.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests.Models
+{
+    using AutomatedCar.SystemComponents;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    class CharasteristicsTest
+    {
+        [TestMethod]
+        [DataRow(1, 3.79)]
+        [DataRow(2, 2.07)]
+        [DataRow(3, 1.36)]
+        [DataRow(4, 1.03)]
+        [DataRow(5, 1)]
+        [DataRow(10, 1)]
+        public void TestGetGearRatio(int gear, double result)
+        {
+            VirtualFunctionBus vfb = new VirtualFunctionBus();
+            vfb.GearboxPacket.InnerGear = gear;
+            Characteristics c = new Characteristics(vfb);
+            var currentGearRatio = c.GetGearRatio();
+            Assert.AreEqual(currentGearRatio, result);
+        }
+
+
+
+
+    }
+}
