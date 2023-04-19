fc3b3ea778c52ac6743fd424ca7cb652ef854b72
andrewhertelendy <hertelendyandras@stud.uni-obuda.hu>
2023-04-19 17:39:39 +0200

    Tamás - Class simplified

--- src/AutomatedCar/SystemComponents/Characteristics.cs
+++ src/AutomatedCar/SystemComponents/Characteristics.cs
@@ -1,91 +1,82 @@
 ﻿namespace AutomatedCar.SystemComponents
 {
-
-    using AutomatedCar.SystemComponents.Packets;
     using System;
+    using AutomatedCar.SystemComponents.Packets;
+
     public class Characteristics : SystemComponent
     {
-        private ICharacteristicsInterface characteristicsPacket;
-        private IGearboxInterface gearbox;
+        private const double TireDiameter = 24; // 24-inch wheel size converted to circumference
+        private const double NaturalDecelerationRate = 10;
+        private const double KmphToMphRatio = 1.609344; // The conversion ratio between kilometer/h and mile/h
+
+        private CharacteristicsPacket characteristicsPacket;
+        private byte lastInnerGear;
+        private double lastGearRatio;
+
         private BrakePedalPacket brakePedalPacket;
         private GasPedalPacket gasPedalPacket;
 
-        private const double WheelCircumference = 24 * Math.PI; // 24-inch wheel size converted to circumference
-        private const double BrakeDeceleration = 9; // Deceleration rate 9m/s^2
-
-        private double tick; // Inner tick variable
-
-        public Characteristics(VirtualFunctionBus virtualFunctionBus, IGearboxInterface gearbox, BrakePedalPacket brakePedalPacket, GasPedalPacket gasPedalPacket)
-             : base(virtualFunctionBus)
+        public Characteristics(VirtualFunctionBus virtualFunctionBus, BrakePedalPacket brakePedalPacket, GasPedalPacket gasPedalPacket)
+            : base(virtualFunctionBus)
         {
             this.characteristicsPacket = new CharacteristicsPacket();
-            this.gearbox = gearbox;
+            virtualFunctionBus.CharacteristicsPacket = this.characteristicsPacket;
+            this.lastInnerGear = this.virtualFunctionBus.GearboxPacket.InnerGear;
+            this.lastGearRatio = this.GetGearRatio();
+            this.characteristicsPacket.Speed = 0;
+            this.characteristicsPacket.RPM = 0;
+
             this.brakePedalPacket = brakePedalPacket;
             this.gasPedalPacket = gasPedalPacket;
-            virtualFunctionBus.CharacteristicsPacket = this.characteristicsPacket;
-            tick = 0;
         }
 
         public override void Process()
         {
-            tick += 1.0 / 60; // Increment tick by 1/60 (assuming this method is called every 1/60th of a second)
+            double currentGearRatio = this.GetGearRatio();
+            double currentSpeed = this.characteristicsPacket.Speed;
+            byte gasPedalState = gasPedalPacket.PedalPosition;
+            byte brakePedalState = brakePedalPacket.PedalPosition;
 
-            double gearFactor = GetGearFactor(gearbox.InnerGear);
-            double acceleration = CalculateAcceleration(gasPedalPacket.GasPedalState, gearFactor, tick);
-            double deceleration = CalculateDeceleration(brakePedalPacket.BrakePedalState, tick);
-
-            double netAcceleration = acceleration - deceleration;
-            characteristicsPacket.Speed = CalculateSpeed(characteristicsPacket.Speed, netAcceleration, tick);
-            characteristicsPacket.RPM = CalculateRPM(characteristicsPacket.Speed);
+            this.characteristicsPacket.RPM = CalculateRPM(currentGearRatio) +
+                CalculateRPMDifference(gasPedalState, brakePedalState, currentGearRatio);
+            this.characteristicsPacket.Speed = CalculateSpeed(currentGearRatio);
         }
 
-        private double GetGearFactor(int innerGear)
+        private double GetGearRatio()
         {
-            switch (innerGear)
+            byte currentInnerGear = this.virtualFunctionBus.GearboxPacket.InnerGear;
+            switch (currentInnerGear)
             {
                 case 1:
-                    return 1.0;
+                    return 3.79;
                 case 2:
-                    return 0.8;
+                    return 2.07;
                 case 3:
-                    return 0.6;
+                    return 1.36;
                 case 4:
-                    return 0.4;
+                    return 1.03;
                 case 5:
-                    return 0.2;
+                    return 1;
                 default:
-                    return 1.0;
+                    return 1;
             }
         }
 
-        private double CalculateAcceleration(int gasPedalState, double gearFactor, double timeDelta)
+        private int CalculateRPMDifference(int gasPedalState, int breakPedalState, double currentGearRatio)
         {
-            return Math.Sqrt(gasPedalState * gearFactor) * timeDelta;
+            return (int)Math.Sqrt((gasPedalState - breakPedalState - NaturalDecelerationRate) * currentGearRatio) * 5;
         }
 
-        private double CalculateDeceleration(int brakePedalState, double timeDelta)
+        private int CalculateRPM(double currentGearRatio)
         {
-            if (brakePedalState > 0)
-            {
-                return BrakeDeceleration * (brakePedalState / 100.0) * timeDelta;
-            }
-            else
-            {
-                return 0;
-            }
+            double currentSpeed = this.characteristicsPacket.Speed / KmphToMphRatio; // Speed need to be in mph in the calculation
+            return (int)(Math.Round(currentSpeed * currentGearRatio * 336) / TireDiameter);
         }
 
-        private float CalculateSpeed(float currentSpeed, double netAcceleration, double timeDelta)
+        private double CalculateSpeed(double currentGearRatio)
         {
-            double newSpeed = currentSpeed + netAcceleration * timeDelta * 3.6; // Convert m/s to km/h
-            return (float)Math.Max(0, newSpeed); // Ensure the speed doesn't go below 0
-        }
-
-        private float CalculateRPM(float speed)
-        {
-            double speedInMetersPerSecond = speed / 3.6; // Convert km/h to m/s
-            double wheelRotationsPerSecond = speedInMetersPerSecond / WheelCircumference;
-            return (float)(wheelRotationsPerSecond * 60); // Convert wheel rotations per second to RPM
+            double currentRPM = this.characteristicsPacket.RPM;
+            return ((TireDiameter * currentRPM) / (currentGearRatio * 366)) * KmphToMphRatio;
         }
     }
 }
