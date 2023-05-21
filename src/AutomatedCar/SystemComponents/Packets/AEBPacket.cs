d9ccaa1e9da8ec3754533f915a63b11bfbf7a326
Martin Pintér <pmrtn@stud.uni-obuda.hu>
2023-05-21 17:18:12 +0200

    AEB interface and packet added

---
+++ src/AutomatedCar/SystemComponents/Packets/AEBPacket.cs
@@ -0,0 +1,21 @@
+﻿namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class AEBPacket : ReactiveObject, IAEBInterface
+    {
+        private bool _AEBIsActive;
+
+        public bool AEBIsActive
+        {
+            get => this._AEBIsActive;
+            set => this.RaiseAndSetIfChanged(ref this._AEBIsActive, value);
+        }
+    }
+}
