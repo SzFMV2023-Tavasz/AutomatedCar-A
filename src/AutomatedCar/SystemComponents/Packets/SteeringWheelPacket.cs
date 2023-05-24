af913bf8abca3803acab327b330c1ab07e7f008e
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-05-24 23:22:30 +0200

    Dashbord wheel rotation implemented

--- src/AutomatedCar/SystemComponents/Packets/SteeringWheelPacket.cs
+++ src/AutomatedCar/SystemComponents/Packets/SteeringWheelPacket.cs
@@ -11,11 +11,18 @@
     public class SteeringWheelPacket : ReactiveObject, ISteeringWheel
     {
         private Vector2D vector2D;
+        private float rotatePosition;
 
         public Vector2D DirectionVector
         {
             get => this.vector2D;
             set => this.RaiseAndSetIfChanged(ref this.vector2D, value);
+        }
+
+        public float RotatePosition
+        {
+            get => this.rotatePosition;
+            set => this.RaiseAndSetIfChanged(ref this.rotatePosition, value);
         }
     }
 }
