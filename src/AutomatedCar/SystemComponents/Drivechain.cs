0ec414b7797c3aeba9cf83ff1fffacacbd618c8b
Martin Pintér <pmrtn@stud.uni-obuda.hu>
2023-05-24 18:56:34 +0200

    Car drifting fixed

--- src/AutomatedCar/SystemComponents/Drivechain.cs
+++ src/AutomatedCar/SystemComponents/Drivechain.cs
@@ -30,65 +30,50 @@
 
         public override void Process()
         {
-
             float speedKMH = this.characteristicsPacket.Speed; // given speed in km/h
             float speedMS = (float)(speedKMH / 3.6); // convert speed to m/s
 
             Vector2 direction = new Vector2(this.SteeringWheelPacket.DirectionVector.X, this.SteeringWheelPacket.DirectionVector.Y); // given direction vector
 
             float turningRadius = float.MaxValue; // set the turning radius to a large number initially
+            float angularVelocity = 0;
 
             // calculate angular velocity in radians per second
             if (direction != Vector2.Zero)
             {
-                float angularVelocity;
-
-                if ((direction.X == 0 && direction.Y == 1) || (direction.X == 0 && direction.Y == -1))
-                {
-                    angularVelocity = 0.0f;
-                }
-
-                else
-                {
-                    angularVelocity = (float)(speedMS / direction.Length());
-                }
-
+                angularVelocity = speedMS / turningRadius;
                 if (angularVelocity != 0)
                 {
-                    turningRadius = (float)(speedMS / angularVelocity);
+                    turningRadius = speedMS / angularVelocity;
                 }
             }
 
             float angle = (float)Math.Atan2(direction.Y, direction.X);
 
-            // calculate motion vector
+            // calculate motion vector with a -90-degree rotation
             Vector2 motion = new Vector2(
                 (float)(speedMS * Math.Cos(angle - (Math.PI / 2.0))),
-                (float)(speedMS * Math.Sin(angle - (Math.PI / 2.0)))) * (float)this.timeBetweenFrames * 30; // motion vector in meters
+                (float)(speedMS * Math.Sin(angle - (Math.PI / 2.0))));
 
             // calculate new position
-            Vector2 newPosition = new Vector2(World.Instance.ControlledCar.X, World.Instance.ControlledCar.Y) + (motion * 2); // * 2 for faster visual acceleration
+            Vector2 newPosition = new Vector2(World.Instance.ControlledCar.X, World.Instance.ControlledCar.Y) +
+                new Vector2((float)(2 * motion.X * this.timeBetweenFrames * 30), (float)(2 * motion.Y * this.timeBetweenFrames * 30));
 
-            // calculate new rotation
-            float newRotation = (float)World.Instance.ControlledCar.Rotation;
+            // calculate new rotation only if there is a change in direction
+            float angleChange = 0;
 
-            if (turningRadius != float.MaxValue)
+            if (turningRadius != float.MaxValue && angularVelocity != 0)
             {
-                float angleChange = (float)(speedMS * this.timeBetweenFrames) / turningRadius;
-                newRotation = (float)(angle - angleChange) * 180.0f / (float)Math.PI;
+                angleChange = (float)(angularVelocity * this.timeBetweenFrames);
             }
 
-            Vector2 diff = new(World.Instance.ControlledCar.X - newPosition.X, World.Instance.ControlledCar.Y - newPosition.Y);
-            var a = diff.Length();
-            this.drivechainPacket.vectorDifferentialLength = diff.Length();
-            //Debug.WriteLine("DIFF LENGTH: " + a);
+            float newRotation = (float)(angle - angleChange) * 180.0f / (float)Math.PI;
 
             // update car position and rotation
             World.Instance.ControlledCar.X = newPosition.X;
             World.Instance.ControlledCar.Y = newPosition.Y;
             World.Instance.ControlledCar.Rotation = newRotation;
             World.Instance.ControlledCar.Velocity = (int)speedKMH;
-
         }
     }
 }
